0decb9bcf526c2c1b56d6d3a17b4f07bff87ccea
hvitek@seznam.cz <hvitek@seznam.cz>
2021-03-11 17:26:32 +0100

    unified NamedServiceEntity returned value from getAll() method

--- src/main/java/spring/filemanipulator/controller/FileOperationController.java
+++ src/main/java/spring/filemanipulator/controller/FileOperationController.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import spring.filemanipulator.service.entity.operation.file.FileOperationServiceEntity;
-import spring.filemanipulator.service.operations.FileOperationService;
+import spring.filemanipulator.service.entity.NamedServiceEntity;
+import spring.filemanipulator.service.operation.file.FileOperationService;
 
 import java.util.Collection;
 
@@ -21,8 +21,8 @@ public class FileOperationController {
     }
 
     @GetMapping
-    public Collection<FileOperationServiceEntity> getAll() {
-        Collection<FileOperationServiceEntity> serviceEntities = fileOperationService.getAll();
+    public Collection<NamedServiceEntity> getAll() {
+        Collection<NamedServiceEntity> serviceEntities = fileOperationService.getAllNamedEntities();
         return serviceEntities;
     }
 }
--- src/main/java/spring/filemanipulator/controller/StringOperationController.java
+++ src/main/java/spring/filemanipulator/controller/StringOperationController.java
@@ -1,18 +1,27 @@
 package spring.filemanipulator.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import spring.filemanipulator.service.entity.NamedServiceEntity;
+import spring.filemanipulator.service.operation.string.StringOperationService;
 
 import java.util.Collection;
-import java.util.Collections;
 
 @RestController
 @RequestMapping("/api/string_operations")
 public class StringOperationController {
 
+    private final StringOperationService stringOperationService;
+
+    @Autowired
+    public StringOperationController(StringOperationService stringOperationService) {
+        this.stringOperationService = stringOperationService;
+    }
+
     @GetMapping
-    public Collection<String> getAll() {
-        return Collections.emptyList();
+    public Collection<NamedServiceEntity> getAll() {
+        return stringOperationService.getAllNamedEntities();
     }
 }
