0d1b7eef9783b4caef307b8dba9646230412c70f
hvitek@seznam.cz <hvitek@seznam.cz>
2021-03-11 17:48:15 +0100

    improve comments and iface signature

--- src/main/java/spring/filemanipulator/service/task/manager/TaskScheduler.java
+++ src/main/java/spring/filemanipulator/service/task/manager/TaskScheduler.java
@@ -1,6 +1,8 @@
 package spring.filemanipulator.service.task.manager;
 
 import spring.filemanipulator.entity.TaskEntity;
+import spring.filemanipulator.service.job.JobNotFoundException;
+import spring.filemanipulator.service.task.TaskAlreadyFinishedException;
 import spring.filemanipulator.service.task.TaskNotFoundException;
 import spring.filemanipulator.service.task.TaskNotScheduledException;
 
@@ -9,5 +11,23 @@ import spring.filemanipulator.service.task.TaskNotScheduledException;
  */
 public interface TaskScheduler {
     void scheduleAndStore(TaskEntity createdTaskEntity);
-    void signalToStop(int taskId) throws TaskNotFoundException, TaskNotScheduledException;
+
+    /**
+     * @param taskId
+     * @throws TaskNotFoundException
+     * @throws TaskNotScheduledException Simply put a Task has not yet been scheduled, therefore it cannot be stopped.
+     * There is a Task record (TaskEntity) in DB, but no related Job record (JobEntity).
+     * The JobEntity record is always created when a new Job is scheduled.
+     *
+     * @throws TaskAlreadyFinishedException If the task has already finished its execution, it, well, does not make sense
+     * to stop...
+     *
+     * @throws JobNotFoundException This should never happen. Indicates DB inconsistency.
+     * Mapping TaskEntity <-> JobEntity corrupted.
+     */
+    void signalToStopThrow(int taskId) throws
+            TaskNotFoundException,
+            TaskNotScheduledException,
+            TaskAlreadyFinishedException,
+            JobNotFoundException;
 }
