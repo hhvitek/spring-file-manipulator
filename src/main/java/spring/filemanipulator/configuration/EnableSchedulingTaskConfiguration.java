f0f767f0633f76b6ada10a43b0b5b9c9baee5dd3
hvitek@seznam.cz <hvitek@seznam.cz>
2021-03-11 17:22:59 +0100

    removed unnecessary imports

--- src/main/java/spring/filemanipulator/configuration/EnableSchedulingTaskConfiguration.java
+++ src/main/java/spring/filemanipulator/configuration/EnableSchedulingTaskConfiguration.java
@@ -1,10 +1,7 @@
 package spring.filemanipulator.configuration;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 /**
  * Enables @Scheduled annotation.
